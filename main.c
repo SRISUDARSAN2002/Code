9b5494372cd8c723106d3d5c74d93a3fb978e38a
Sri Sudarsan <srisudarsan2002@gmail.com>
2025-07-18 22:37:26 +0530

    status added

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+    int status = 0;
+
+    status |= 0;
+     printf(" status1 = %d\n", status);
+    status |= 0;
+     printf(" status2 = %d\n", status);
+    status |= 1;
+     printf(" status3 = %d\n", status);
+    status |= 0;
+     printf(" status4 = %d\n", status);
+    status |= 0;
+     printf(" status5 = %d\n", status);
+
+    return status;
 }
