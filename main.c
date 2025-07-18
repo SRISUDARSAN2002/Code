b30e39fc6bf2267dfdf6ed5182cfa51afa098024
Sri Sudarsan <srisudarsan2002@gmail.com>
2025-07-18 22:45:23 +0530

    status change

--- main.c
+++ main.c
@@ -5,15 +5,15 @@ int main() {
 
     int status = 0;
 
-    status |= 0;
+    status &= 0;
      printf(" status1 = %d\n", status);
-    status |= 0;
+    status &= 0;
      printf(" status2 = %d\n", status);
-    status |= 1;
+    status &= 1;
      printf(" status3 = %d\n", status);
-    status |= 0;
+    status &= 0;
      printf(" status4 = %d\n", status);
-    status |= 0;
+    status &= 0;
      printf(" status5 = %d\n", status);
 
     return status;
